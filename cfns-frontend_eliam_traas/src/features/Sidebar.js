139ddf7c555bdfeb178a5a5cff62c6fcefad0df0
Elipam <eliamtraas@gmail.com>
2024-03-22 14:51:54 +0100

    npm installs gedaan en nu doet het het weer

--- cfns-frontend_eliam_traas/src/features/Sidebar.js
+++ cfns-frontend_eliam_traas/src/features/Sidebar.js
@@ -1,126 +1,23 @@
 import React, { useState } from "react";
-import styled from "styled-components";
-import { Link } from "react-router-dom";
+import { IconContext } from "react-icons/lib";
 import * as FaIcons from "react-icons/fa";
 import * as AiIcons from "react-icons/ai";
-import { IconContext } from "react-icons/lib";
 import checkmarkImage from "../utility/checkmark.png";
-
-// Styled components voor de Navbar
-// Zwarte balk bovenaan de pagina
-const Nav = styled.div`
-  background: #333333; // Achtergrondkleur van de balk
-  height: 60px; // Hoogte van de balk
-  display: flex; // Flexbox layout
-  align-items: center; // Gecentreerde uitlijning van items
-`;
-
-// Gebruikt om de zijbalk te openen
-const NavIcon = styled(Link)`
-  margin-left: 20px; // Linkermarge
-  font-size: 30px; // Tekstgrootte
-  height: 80px; // Hoogte van de pictogram
-  display: flex; // Flexbox layout
-  align-items: center; // Gecentreerde uitlijning van items
-  color: white; // Pictogramkleur
-`;
-
-// Blauwe CFNS-titel op Nav
-const CFNSTitle = styled.h1`
-  text-align: center; // Gecentreerde uitlijning van tekst
-  color: white; // Tekstkleur
-  margin-left: 250px; // Linkermarge
-`;
-
-// Styled components voor de Sidebar
-const SidebarNav = styled.nav`
-  background: #333333; // Achtergrondkleur van de zijbalk
-  width: 250px; // Breedte van de zijbalk
-  height: 100vh; // Volledige hoogte van het scherm
-  display: flex; // Flexbox layout
-  flex-direction: column; // Stapel kolommen boven op elkaar
-  position: fixed; // Vaste positie op het scherm
-  top: 0; // Afstand vanaf de bovenkant van het scherm
-  left: ${({ sidebar }) => (sidebar ? "0" : "-100%")}; // Positie van de zijbalk (ingeschoven of uitgeschoven)
-  transition: 300ms; // Overgangseffect
-  z-index: 10; // Z-index voor stapeling van elementen
-`;
-
-const CloseButton = styled.div`
-  display: flex; // Flexbox layout
-  justify-content: flex-end; // Uitlijning naar rechts
-  margin-right: 10px; // Ondermarge
-`;
-
-// Styled components voor de ProviderButtons
-const ProviderButtonsContainer = styled.div`
-  display: grid; // Grid layout
-  grid-template-columns: repeat(2, 1fr); // Grid kolommen (2 kolommen)
-  grid-gap: 10px; // Ruimte tussen de kolommen
-  justify-items: center; // Gecentreerde uitlijning van items
-  margin-top: 40px; // Bovenmarge
-  margin-bottom: 50px; // Ondermarge
-`;
-
-const ProviderButton = styled.button`
-  width: 100px; // Breedte van de knop
-  height: 40px; // Hoogte van de knop
-  margin-bottom: 20px; // Ondermarge
-  font-size: 16px; // Tekengrootte
-  background-color: ${({ isActive }) => (isActive ? 'darkblue' : 'blue')}; // Achtergrondkleur van de knop
-  color: white; // Tekleur
-  border: none; // Geen rand
-  border-radius: 5px; // Afronding van de hoeken
-  cursor: pointer; // Wijziging van de cursor bij hover
-`;
-
-// Styled components voor de Checkbox en Slider
-const CheckboxSliderContainer = styled.div`
-  display: flex; // Flexbox layout
-  align-items: center; // Gecentreerde uitlijning van items
-  margin-bottom: 20px;
-`;
-
-const CheckButton = styled.button`
-  width: 20px; // Breedte van de knop
-  height: 20px; // Hoogte van de knop
-  margin-right: 20px; // Rechtermarge
-  margin-left: 20px; // Linkermarge
-  background-color: #333333; // Achtergrondkleur van de knop
-  color: white; // Tekstkleur
-  border: 1px solid white; // Randstijl
-  border-radius: 5px; // Afronding van de hoeken
-  cursor: pointer; // Wijziging van de cursor bij hover
-`;
-
-const CheckImage = styled.img`
-  width: 15px; // Breedte van de afbeelding
-  height: 15px; // Hoogte van de afbeelding
-  margin-left: -5px; // Linkermarge (negatief om naar links te verplaatsen)
-`;
-
-const SliderContainer = styled.div`
-  width: 180px; // Breedte van de container
-`;
-
-const Slider = styled.input`
-  width: 100%; // Volledige breedte van de container
-  height: 100%; // Volledige hoogte van de container
-  transition: opacity 0.2s; // Overgangseffect voor de slider
-
-  &:hover {
-    opacity: 1; // Ondoorzichtigheid bij hover
-  }
-
-  &::-webkit-slider-thumb {
-    cursor: pointer; // Wijziging van de cursor bij hover
-  }
-`;
-
-const SliderLabel = styled.div`
-  color: white; // Tekstkleur
-  text-align: center; // Gecentreerde uitlijning van tekst
-`;
+import {
+  Nav,
+  NavIcon,
+  CFNSTitle,
+  SidebarNav,
+  CloseButton,
+  ProviderButtonsContainer,
+  ProviderButton,
+  CheckboxSliderContainer,
+  CheckButton,
+  CheckImage,
+  SliderContainer,
+  Slider,
+  SliderLabel
+} from "./StyledComponents";
 
 // Main Sidebar component
 const Sidebar = () => {
@@ -194,7 +91,6 @@ const Sidebar = () => {
               Tampnet
             </ProviderButton>
           </ProviderButtonsContainer>
-          {/* Eerste CheckboxSliderContainer */}
           <CheckboxSliderContainer>
             <CheckButton onClick={handleCheckboxChange1}>
               {isChecked1 && <CheckImage src={checkmarkImage} alt="Checkmark" />}
@@ -210,7 +106,6 @@ const Sidebar = () => {
               <SliderLabel>Golfhoogte: {sliderValue1} cm</SliderLabel>
             </SliderContainer>
           </CheckboxSliderContainer>
-          {/* Tweede CheckboxSliderContainer */}
           <CheckboxSliderContainer>
             <CheckButton onClick={handleCheckboxChange2}>
               {isChecked2 && <CheckImage src={checkmarkImage} alt="Checkmark" />}
---
+++ cfns-frontend_eliam_traas/src/features/StyledComponents.js
@@ -0,0 +1,111 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export const Nav = styled.div`
+  background: #333333;
+  height: 60px;
+  display: flex;
+  align-items: center;
+`;
+
+export const NavIcon = styled(Link)`
+  margin-left: 20px;
+  font-size: 30px;
+  height: 80px;
+  display: flex;
+  align-items: center;
+  color: white;
+`;
+
+export const CFNSTitle = styled.h1`
+  text-align: center;
+  color: white;
+  margin-left: 250px;
+`;
+
+export const SidebarNav = styled.nav`
+  background: #333333;
+  width: 250px;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  position: fixed;
+  top: 0;
+  left: ${({ sidebar }) => (sidebar ? "0" : "-100%")};
+  transition: 300ms;
+  z-index: 10;
+`;
+
+export const CloseButton = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  margin-right: 10px;
+`;
+
+export const ProviderButtonsContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 10px;
+  justify-items: center;
+  margin-top: 40px;
+  margin-bottom: 50px;
+`;
+
+export const ProviderButton = styled.button`
+  width: 100px;
+  height: 40px;
+  margin-bottom: 20px;
+  font-size: 16px;
+  background-color: ${({ isActive }) => (isActive ? 'darkblue' : 'blue')};
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
+export const CheckboxSliderContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+
+export const CheckButton = styled.button`
+  width: 20px;
+  height: 20px;
+  margin-right: 20px;
+  margin-left: 20px;
+  background-color: #333333;
+  color: white;
+  border: 1px solid white;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
+export const CheckImage = styled.img`
+  width: 15px;
+  height: 15px;
+  margin-left: -5px;
+`;
+
+export const SliderContainer = styled.div`
+  width: 180px;
+`;
+
+export const Slider = styled.input`
+  width: 100%;
+  height: 100%;
+  transition: opacity 0.2s;
+
+  &:hover {
+    opacity: 1;
+  }
+
+  &::-webkit-slider-thumb {
+    cursor: pointer;
+  }
+`;
+
+export const SliderLabel = styled.div`
+  color: white;
+  text-align: center;
+`;
