0cfd5a720bca6039c76589e4d7f5d6fd208dcf69
Elipam <eliamtraas@gmail.com>
2024-04-06 13:54:54 +0200

    config aan gitignore toegevoegd voor veiligstellen api key

---
+++ cfns-frontend_eliam_traas/src/utility/config.js
@@ -0,0 +1,8 @@
+// config.js
+
+const config = {
+    ARC_GIS_API_KEY: 'AAPKe0e926b6b4e7413aaea425b81eb287c6Dx0BBJiXRPfoDCI2KvvtE3gEkY_O2SdTbwnQhviMjZgrSBIpHajYcYrQKKjLVnN3'
+  };
+  
+  export default config;
+  
